0a16bdd9d0d26f3d9bcda307eebabfeec467d0b5
vidpoint <77872011+vidpoint@users.noreply.github.com>
2021-04-25 00:42:06 +0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "240px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
